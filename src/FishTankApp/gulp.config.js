e9b6f109c7ce71019d65a03d72fb959a9045b856
Igor Ilic <ilicigor@outlook.com>
2016-03-06 22:16:48 +0100

    gulp

---
+++ src/FishTankApp/gulp.config.js
@@ -0,0 +1,33 @@
+﻿module.exports = function () {
+    var webroot = './wwwroot/';
+    var bower = {
+        //json: require('./bower.json'),
+        directory: './bower_components/',
+        ignorePath: '../..'
+    };
+    var config = {
+        webroot: webroot,
+        alljs: [
+            './src/**/*.js',
+            './*.js'
+        ],
+        // CSS LIBS
+        bootstrapCss: bower.directory + 'bootstrap/dist/css/bootstrap.css',
+        sbAdminCss: bower.directory + 'startbootstrap-sb-admin-2/dist/css/sb-admin-2.css',
+        fontAwesomeCss: bower.directory + 'font-awesome/css/font-awesome.css',
+        morrisCss: bower.directory + 'morrisjs/morris.css',
+        // JS LIBS
+        jqueryJs: bower.directory + 'jquery/dist/jquery.js',
+        raphaelJs: bower.directory + 'raphael/raphael.js',
+        morrisJs: bower.directory + 'morrisjs/morris.js',
+        // FONTS LIBS
+        fonts: bower.directory + 'font-awesome/fonts/*',
+        // DEST
+        jsDest: webroot + 'js',
+        cssDest: webroot + 'css',
+        fontDest: webroot + 'fonts'
+        
+    };
+
+    return config;
+}
---
+++ src/FishTankApp/gulpfile.js
@@ -0,0 +1,54 @@
+﻿/// <binding AfterBuild='copy' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
+
+var paths = require('./gulp.config')();
+
+// TASKS
+gulp.task('min:js', function() {
+    return gulp
+        .src([paths.jqueryJs, paths.raphaelJs, paths.morrisJs])
+        .pipe(concat(paths.jsDest + '/min/site.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('copy:js', function() {
+    return gulp
+        .src([paths.jqueryJs, paths.raphaelJs, paths.morrisJs])
+        .pipe(gulp.dest(paths.jsDest));
+});
+
+gulp.task('min:css', function () {
+    return gulp
+        .src([paths.bootstrapCss, paths.sbAdminCss, paths.fontAwesomeCss, paths.morrisCss])
+        .pipe(concat(paths.cssDest + '/min/site.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('copy:css', function () {
+    return gulp
+        .src([paths.bootstrapCss, paths.sbAdminCss, paths.fontAwesomeCss, paths.morrisCss])
+        .pipe(gulp.dest(paths.cssDest));
+});
+
+gulp.task('copy:fonts', function () {
+    return gulp
+        .src([paths.fonts])
+        .pipe(gulp.dest(paths.fontDest));
+});
+
+gulp.task('min', ['min:js', 'min:css']);
+gulp.task('copy', ['copy:js', 'copy:css', 'copy:fonts']);
+
+//gulp.task('default', function () {
+//    // place code for your default task here
+//});
